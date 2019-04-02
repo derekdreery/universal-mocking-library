d1f675e9d89e7802169d74da868a22be0c677987
Richard Dodd (dodj) <richard.o.dodd@gmail.com>
2019-04-02 16:58:13 +0100

    MI

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ lazy_static::lazy_static! {
     static ref MOCKS: Vec<&'static str> = vec![
         "Your mother was a hamster and your father smelt of elderberries.",
         "You have disturbing nasal hair.",
+        "You fight like a diary farmer.",
         "Your pottery skills are lacking.",
         "You are a windows user.",
         "Your headphones cost less than $100",
